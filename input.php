034bff1890b322e0d1bc4dc595e3ea3b3c32f359
amandafidelia <amandafidelia45@gmail.com>
2020-11-17 19:46:38 +0700

    upload tugas

---
+++ input.php
@@ -0,0 +1,6 @@
+<Form method="POST" action="proses.php">
+Bil 1 <input type = "text" id="Bil1" name="Bil1"> <br>
+Bil 2<input type = "text" id="Bil2" name="Bil2"> <br>
+<input type="submit" name="submit" value="PROSES">
+
+</form>
---
+++ proses.php
@@ -0,0 +1,8 @@
+<?php>
+$Bil1=$_POST["Bil1"];
+$Bil2=$_POST["Bil2"];
+
+
+$proses = $Bil1+$Bil2;
+echo "$Bil1 + $Bil2 = $proses";
+?>
